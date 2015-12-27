1dae19665170167d5069801f084217d1d60f975e
helloworld-cgs <genshen220284@foxmail.com>
2015-12-27 13:38:38 +0800

    Update mail.c

--- app/src/main/jni/mail.c
+++ app/src/main/jni/mail.c
@@ -3,10 +3,10 @@
 
 JNIEXPORT jstring JNICALL Java_com_holo_encrypt_MailAccount_getMailAccount
         (JNIEnv *env, jclass obj){
-    return (*env)->NewStringUTF(env, "1690512717@qq.com");
+    return (*env)->NewStringUTF(env, "********");
 }
 
 JNIEXPORT jstring JNICALL Java_com_holo_encrypt_MailAccount_getMailPassword
         (JNIEnv *env, jclass obj){
-    return (*env)->NewStringUTF(env, "gslovemj");
+    return (*env)->NewStringUTF(env, "**********");
 }
